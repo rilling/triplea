3e1f034188b93253986a4cf2918595f7864a3390
nicotowa <nicolastowakamgne@gmail.com>
2026-04-07 16:49:11 -0400

    Refactor: Extract duplicate retreat logic into RetreaterUtil - Fixes #46

--- game-app/game-core/src/main/java/games/strategy/triplea/delegate/battle/steps/retreat/RetreaterPartialAmphibious.java
+++ game-app/game-core/src/main/java/games/strategy/triplea/delegate/battle/steps/retreat/RetreaterPartialAmphibious.java
@@ -6,14 +6,11 @@ import static games.strategy.triplea.delegate.battle.BattleState.UnitBattleFilte
 import games.strategy.engine.data.CompositeChange;
 import games.strategy.engine.data.Territory;
 import games.strategy.engine.data.Unit;
-import games.strategy.engine.data.changefactory.ChangeFactory;
 import games.strategy.triplea.delegate.Matches;
 import games.strategy.triplea.delegate.battle.BattleState;
 import games.strategy.triplea.delegate.battle.MustFightBattle;
-import games.strategy.triplea.formatter.MyFormatter;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import org.triplea.java.collections.CollectionUtils;
@@ -26,15 +23,15 @@ class RetreaterPartialAmphibious implements Retreater {
   @Override
   public Collection<Unit> getRetreatUnits() {
     return CollectionUtils.getMatches(
-        battleState.filterUnits(ALIVE, OFFENSE), Matches.unitWasNotAmphibious());
+            battleState.filterUnits(ALIVE, OFFENSE), Matches.unitWasNotAmphibious());
   }
 
   @Override
   public Collection<Territory> getPossibleRetreatSites(final Collection<Unit> retreatUnits) {
     final Collection<Territory> allRetreatTerritories = battleState.getAttackerRetreatTerritories();
     return retreatUnits.stream().anyMatch(Matches.unitIsLand())
-        ? CollectionUtils.getMatches(allRetreatTerritories, Matches.territoryIsLand())
-        : new ArrayList<>(allRetreatTerritories);
+            ? CollectionUtils.getMatches(allRetreatTerritories, Matches.territoryIsLand())
+            : new ArrayList<>(allRetreatTerritories);
   }
 
   @Override
@@ -49,29 +46,7 @@ class RetreaterPartialAmphibious implements Retreater {
     final CompositeChange change = new CompositeChange();
     final List<RetreatHistoryChild> historyChildren = new ArrayList<>();
 
-    final Collection<Unit> airRetreating =
-        CollectionUtils.getMatches(
-            retreatUnits,
-            Matches.unitIsAir().and(Matches.unitIsOwnedBy(battleState.getPlayer(OFFENSE))));
-
-    if (!airRetreating.isEmpty()) {
-      battleState.retreatUnits(OFFENSE, airRetreating);
-      final String transcriptText = MyFormatter.unitsToText(airRetreating) + " retreated";
-      historyChildren.add(RetreatHistoryChild.of(transcriptText, new ArrayList<>(airRetreating)));
-    }
-
-    final Collection<Unit> nonAirRetreating = new HashSet<>(retreatUnits);
-    nonAirRetreating.removeAll(airRetreating);
-    nonAirRetreating.addAll(battleState.getDependentUnits(nonAirRetreating));
-
-    if (!nonAirRetreating.isEmpty()) {
-      battleState.retreatUnits(OFFENSE, nonAirRetreating);
-      historyChildren.add(
-          RetreatHistoryChild.of(
-              MyFormatter.unitsToText(nonAirRetreating) + " retreated to " + retreatTo.getName(),
-              new ArrayList<>(nonAirRetreating)));
-      change.add(ChangeFactory.moveUnits(battleState.getBattleSite(), retreatTo, nonAirRetreating));
-    }
+    RetreaterUtil.applyRetreatChanges(retreatTo, retreatUnits, battleState, change, historyChildren);
 
     return RetreatChanges.of(change, historyChildren);
   }
